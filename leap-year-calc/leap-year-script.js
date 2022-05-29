820e01150149bf91b0a6c7cb38dbb1001aebe55c
Taylor Casse <taylorcasse@gmail.com>
2022-05-29 09:45:59 +0200

    Finished leap year calculator

--- leap-year-calc/leap-year-script.js
+++ leap-year-calc/leap-year-script.js
@@ -1,5 +1,18 @@
 // Leap year calculator for fun and to prove a point
-// import {sanitizeIntiger} from './useful-functions.js'
+// import './useful-functions.js'
+
+function sanitizeInteger(input) {
+    let isNumber
+    for (let i = 0; i < input.length; i++) {
+        if (isNaN(input) || input === '') {
+            
+        } else {
+            return input;
+    }
+
+    }
+}
+
 
 document.getElementById('leap-button').addEventListener('click', () => {
     console.log('button clicked');
@@ -22,13 +35,12 @@ document.getElementById('leap-button').addEventListener('click', () => {
     }
 
     // Initialize variables
-    const startYear = document.getElementById("leap-input-start").value;
-    const endYear = document.getElementById("leap-input-end").value;
+    const startYear = sanitizeInteger(document.getElementById("leap-input-start").value);
+    // const startYear = document.getElementById("leap-input-start").value;
+    const endYear = sanitizeInteger(document.getElementById("leap-input-end").value);
     const yearDiff = endYear - startYear;
-    const yearArray = [];
+    const yearArray = [isLeapYear(startYear)];
     const output = document.getElementById("leap-output");
-    
-    
 
     // Generate an array of outputs
     for (let currentYear = startYear; currentYear <= endYear; currentYear++) {
@@ -36,6 +48,7 @@ document.getElementById('leap-button').addEventListener('click', () => {
     }
 
     output.innerHTML = yearArray.join(' ');
-
-
 })
+
+
+
