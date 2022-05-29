2667c47f6d153d24e8aec0083d98ebeb6827b91f
Taylor Casse <taylorcasse@gmail.com>
2022-05-29 10:55:19 +0200

    Added prime number checker and generator

---
+++ prime/prime-script.js
@@ -0,0 +1,56 @@
+
+
+function primeCheck(num) {
+    // Checks if num is prime by checking the mod of num and all numbers smaller than num
+    // excluding 0 and 1
+    let isPrime = true;
+    for (let i = 0; i < num; i++) {
+        if (num % i === 0 && ![0,1].includes(i)) {
+            isPrime = false;
+        }
+    }
+    let primeString;
+    (isPrime) ? primeString = 'Prime' : primeString = 'Not prime'
+    return primeString;
+}
+
+function primeGen(startNum, endNum) {
+    // Returns all prime numbers from startNum to endNum
+    const primeArr = [];
+    for (let currentNum = startNum; currentNum <= (endNum - startNum); currentNum++) {
+        if (primeCheck(currentNum)) {
+            primeArr.push(currentNum);
+        }
+    }
+    return primeArr.join(' ');
+}
+
+document.getElementById('prime-check-button').addEventListener('click', () => {
+    console.log('prime-check');
+    // Initialize inputs
+    const input = document.getElementById('prime-check-input').value;
+
+    // Sanitize and output
+    if (isNaN(input)) {
+        alert('Please enter a valid number');
+    } else {
+        document.getElementById('prime-check-output').innerHTML = `${input}: ${primeCheck(input)}`
+    }
+});
+
+document.getElementById('prime-gen-button').addEventListener('click', () => {
+    
+    // Initialize inputs
+    const startNum = document.getElementById('prime-gen-start').value;
+    const endNum = document.getElementById('prime-gen-end').value;
+
+    // Sanitize
+    if (isNaN(startNum) || isNaN(endNum)) {
+        alert('Please enter valid numbers');
+    } else if (+startNum >= +endNum) {
+        alert('Please enter a valid range');
+    } else {
+        document.getElementById('prime-gen-output').innerHTML = primeGen(startNum, endNum);
+    }
+
+});
