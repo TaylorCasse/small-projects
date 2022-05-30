66e7865bc33241474fc1f2b0be36304d93cb75e4
Taylor Casse <taylorcasse@gmail.com>
2022-05-30 20:02:39 +0200

    Added input checking

--- js-projects-site/leap-year-calc/leap-year-script.js
+++ js-projects-site/leap-year-calc/leap-year-script.js
@@ -4,50 +4,65 @@
 function sanitizeInteger(input) {
     let isNumber
     for (let i = 0; i < input.length; i++) {
-        if (isNaN(input) || input === '') {
-            
+        if (isNaN(input) || input === '') {       
         } else {
             return input;
-    }
-
+        }
     }
 }
 
-
-document.getElementById('leap-button').addEventListener('click', () => {
-    console.log('button clicked');
-    function isLeapYear(year) {
-        // Calculates if the year argument is a leap year
-        console.log('function run')
-        if ((year % 4) === 0) {
-            if (year % 100 === 0) {
-                if (year % 400 === 0) {
-                    return 'Leap year.';
-                } else {
-                    return 'Not a leap year.';
-                }
-            } else {
+function isLeapYear(year) {
+    // Calculates if the year argument is a leap year
+    console.log('function run')
+    if ((year % 4) === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
                 return 'Leap year.';
+            } else {
+                return 'Not a leap year.';
             }
         } else {
-            return 'Not a leap year.';
+            return 'Leap year.';
         }
+    } else {
+        return 'Not a leap year.';
     }
+}
+
+document.getElementById('leap-button-1').addEventListener('click', () => {
+    const year = document.getElementById('leap-input').value;
+    if (year === '') {
+        alert('Please enter a year to check');
+
+    } else if (isNaN(year) || year < 0) {
+        alert('Please enter a valid year');
+    } else {
+        document.getElementById('leap-output-1').innerHTML = isLeapYear(year);
+    }
+})
+
+document.getElementById('leap-button-2').addEventListener('click', () => {
 
     // Initialize variables
     const startYear = sanitizeInteger(document.getElementById("leap-input-start").value);
-    // const startYear = document.getElementById("leap-input-start").value;
     const endYear = sanitizeInteger(document.getElementById("leap-input-end").value);
-    const yearDiff = endYear - startYear;
-    const yearArray = [isLeapYear(startYear)];
-    const output = document.getElementById("leap-output");
+    const yearArray = [];
+    const output = document.getElementById("leap-output-2");
 
-    // Generate an array of outputs
-    for (let currentYear = startYear; currentYear <= endYear; currentYear++) {
-        yearArray.push(`${currentYear}: ${isLeapYear(currentYear)}</br>`);
+    // Check inputs
+    if (startYear === '' || endYear === '') {
+        alert('Please enter a start year and end year');
+    } else if (isNaN(startYear) || isNaN(endYear) || startYear < 0 || endYear < 0) {
+        alert('Please enter valid years');
+    } else if (endYear <= startYear) {
+        alert('The end year must be greater than the start year');
+    } else {
+        // Generate an array of outputs
+        for (let currentYear = startYear; currentYear <= endYear; currentYear++) {
+            yearArray.push(`${currentYear}: ${isLeapYear(currentYear)}</br>`);
+        }
+        output.innerHTML = yearArray.join(' ');
     }
-
-    output.innerHTML = yearArray.join(' ');
 })
 
 
